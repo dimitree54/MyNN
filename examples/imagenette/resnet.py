bdcdc5e105cbe01dd78e0ef9e8d705e9c543fe13
Dmitrii Rashchenko <dimitree54@gmail.com>
2020-05-31 16:03:12 +0300

    saving only last ckpt

--- examples/imagenette/resnet.py
+++ examples/imagenette/resnet.py
@@ -9,23 +9,20 @@ NF = 64
 BS = 32
 
 
-def get_epoch_lr_from_checkpoint_name(name):
+def get_epoch_from_checkpoint_name(name):
     without_path = os.path.split(name)[1]
-    epoch_str, lr_str = without_path.split('-')
-    return int(epoch_str), float(lr_str)
+    return int(without_path)
 
 
 if __name__ == "__main__":
     logs_dir = os.path.join("resnet50")
-    checkpoint_path = os.path.join(logs_dir, "{epoch}-{lr:.5f}")
+    checkpoint_path = os.path.join(logs_dir, "{epoch}")
     tensorboard_path = os.path.join(logs_dir, "tensorboard")
     export_path = os.path.join(logs_dir, "export")
     backbone_export_path = os.path.join(export_path, "backbone")
     head_export_path = os.path.join(export_path, "head")
 
     train_batches, validation_batches = get_data(BS)
-    train_batches = train_batches
-    validation_batches = validation_batches
 
     backbone = get_resnet50_backbone(NF)
     head = ClassificationHeadBuilder().build(10)
@@ -33,22 +30,22 @@ if __name__ == "__main__":
 
     lr_schedule = tf.keras.callbacks.ReduceLROnPlateau()
     tensorboard = tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path)
-    saver = tf.keras.callbacks.ModelCheckpoint(checkpoint_path)
+    saver = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, monitor="loss", save_best_only=True)
 
     latest_checkpoint = tf.train.latest_checkpoint(logs_dir)
     if latest_checkpoint:
-        prev_epoch, init_lr = get_epoch_lr_from_checkpoint_name(latest_checkpoint)
+        prev_epoch = get_epoch_from_checkpoint_name(latest_checkpoint)
     else:
-        prev_epoch, init_lr = 0, 0.1
+        prev_epoch = 0
 
-    model.compile(tf.keras.optimizers.SGD(init_lr, momentum=0.9),
+    model.compile(tf.keras.optimizers.SGD(0.1, momentum=0.9),
                   tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                   tf.keras.metrics.SparseCategoricalAccuracy())
 
     if latest_checkpoint:
         model.load_weights(latest_checkpoint)
 
-    history = model.fit(train_batches, epochs=100, callbacks=[lr_schedule, tensorboard, saver],
+    history = model.fit(train_batches, epochs=1000, callbacks=[lr_schedule, tensorboard, saver],
                         initial_epoch=prev_epoch, validation_data=validation_batches)
 
     backbone.save(backbone_export_path, include_optimizer=False)

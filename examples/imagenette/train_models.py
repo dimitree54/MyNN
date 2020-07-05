7a19b89337233a1b903b7960b853c9c4229799e9
Dmitrii Rashchenko <dimitree54@gmail.com>
2020-07-05 12:38:33 +0300

    train functions for GC

--- examples/imagenette/train_models.py
+++ examples/imagenette/train_models.py
@@ -7,7 +7,7 @@ from models.architectures.resnext import get_resnext50_backbone
 from models.architectures.resnet_with_attention import get_ge_resnet50_backbone, get_se_resnet50_backbone, \
     get_xse_resnext50_backbone, \
     get_xse_resnet50_backbone, get_xge_resnet50_backbone, get_xge_resnext50_backbone, get_nl_resnet50_backbone, \
-    get_xnl_resnet50_backbone
+    get_xnl_resnet50_backbone, get_gc_resnet50_backbone, get_xgc_resnet50_backbone
 from models.architectures.xresnet import get_x_resnet50_backbone, get_x_resnext50_backbone
 from models.base_classes import ClassificationHeadBuilder, ClassificationModel
 
@@ -216,6 +216,42 @@ def train_xnl_resnet50():
     name = "xnl_resnet50"
 
     xresnet_backbone = get_xnl_resnet50_backbone(nf)
+    head = ClassificationHeadBuilder().build(10)
+    model = ClassificationModel(xresnet_backbone, head)
+
+    train_batches, validation_batches = get_data(bs)
+
+    train(model, name, train_batches, validation_batches)
+
+    export_path = os.path.join(name, "export")
+    backbone_export_path = os.path.join(export_path, "backbone")
+    xresnet_backbone.save(backbone_export_path, include_optimizer=False)
+
+
+def train_gc_resnet50():  # TODO not trained
+    nf = 64
+    bs = 32
+    name = "gc_resnet50"
+
+    resnet_backbone = get_gc_resnet50_backbone(nf)
+    head = ClassificationHeadBuilder().build(10)
+    model = ClassificationModel(resnet_backbone, head)
+
+    train_batches, validation_batches = get_data(bs)
+
+    train(model, name, train_batches, validation_batches)
+
+    export_path = os.path.join(name, "export")
+    backbone_export_path = os.path.join(export_path, "backbone")
+    resnet_backbone.save(backbone_export_path, include_optimizer=False)
+
+
+def train_xgc_resnet50():  # TODO not trained
+    nf = 64
+    bs = 32
+    name = "xgc_resnet50"
+
+    xresnet_backbone = get_xgc_resnet50_backbone(nf)
     head = ClassificationHeadBuilder().build(10)
     model = ClassificationModel(xresnet_backbone, head)
 
